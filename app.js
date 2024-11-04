ad4b893b0c79e4296b6bc8988f7a7b6ae91ef081
prinsi-t <tprincy56@gmail.com>
2024-11-04 20:42:52 +0530

    bb

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ app.use(express.static("public")); // Serve static files
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, { 
     useNewUrlParser: true, 
-    useUnifiedTopology: true 
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 40000
   })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Failed to connect to MongoDB", err));
